863eaba8b7f9cab8ae08805373b8413da433cf79
BuildTools <unconfigured@null.spigotmc.org>
2019-03-09 10:45:53 +0200

    Added Category and Vendor with Bootstrap.

---
+++ src/main/java/blue/springframework/spring5webfluxrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package blue.springframework.spring5webfluxrest.repositories;
+
+import blue.springframework.spring5webfluxrest.domain.Category;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface CategoryRepository extends ReactiveMongoRepository<Category, String> {
+}
